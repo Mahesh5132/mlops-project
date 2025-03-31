6a787f390fccdcdaf5fd7aa0b2934b3752d4ef6a
Mahesh <patilmahesh5132@gmail.com>
2025-03-31 13:35:29 +0530

    ci.yml path tracker

--- mlflow_artifact_uri.py
+++
@@ -1,5 +0,0 @@
-from mlflow.tracking import MlflowClient
-client = MlflowClient()
-runs = client.search_runs(experiment_ids=["0"], order_by=["start_time desc"])
-for run in runs:
-    print(run.info.artifact_uri)
